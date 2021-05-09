e896a61c55e286c7ee94b44433c67d8dfe243d31
Keith Dluu <dangkhoa240899@gmail.com>
2021-05-09 10:45:43 +0700

    Set up service discovery and externalized config

--- src/main/java/com/filmbooking/service_discoverer/ServiceDiscovererApplication.java
+++ src/main/java/com/filmbooking/service_discoverer/ServiceDiscovererApplication.java
@@ -2,8 +2,10 @@ package com.filmbooking.service_discoverer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscovererApplication {
 
 	public static void main(String[] args) {
