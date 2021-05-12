00fcfc5614c63044fcc1bfe98ae9b9258d3d269e
Keith Dluu <dangkhoa240899@gmail.com>
2021-05-12 23:54:37 +0700

    Config discovery service

--- src/test/java/com/filmbooking/service_discoverer/ServiceDiscovererApplicationTests.java
+++ src/test/java/com/filmbooking/service_discoverer/ServiceDiscovererApplicationTests.java
@@ -3,7 +3,9 @@ package com.filmbooking.service_discoverer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  properties = {"spring.cloud.config.enabled=false"}
+)
 class ServiceDiscovererApplicationTests {
 
 	@Test
